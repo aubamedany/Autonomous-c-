e807b665c4dc9d8624d128ad97afb51a680a2697
aubamedany <lenguyenhaidang14@gmail.com>
2024-04-21 12:22:43 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,100 @@
+"""
+Lane Lines Detection pipeline
+
+Usage:
+    main.py [--video] INPUT_PATH OUTPUT_PATH 
+
+Options:
+
+-h --help                               show this screen
+--video                                 process video file instead of image
+"""
+
+import numpy as np
+import matplotlib.image as mpimg
+import cv2
+from docopt import docopt
+from IPython.display import HTML, Video
+from moviepy.editor import VideoFileClip
+from CameraCalibration import CameraCalibration
+from Thresholding import *
+from PerspectiveTransformation import *
+from LaneLines import *
+
+class FindLaneLines:
+    """ This class is for parameter tunning.
+
+    Attributes:
+        ...
+    """
+    def __init__(self):
+        """ Init Application"""
+        self.calibration = CameraCalibration('camera_cal', 9, 6)
+        self.thresholding = Thresholding()
+        self.transform = PerspectiveTransformation()
+        self.lanelines = LaneLines()
+
+    def forward(self, img):
+        out_img = np.copy(img)
+        img = self.calibration.undistort(img)
+        img = self.transform.forward(img)
+        img = self.thresholding.forward(img)
+        img = self.lanelines.forward(img)
+        img = self.transform.backward(img)
+
+        out_img = cv2.addWeighted(out_img, 1, img, 0.6, 0)
+        out_img = self.lanelines.plot(out_img)
+        return out_img
+
+    def process_image(self, input_path, output_path):
+        img = mpimg.imread(input_path)
+        out_img = self.forward(img)
+        mpimg.imsave(output_path, out_img)
+        return out_img
+
+    def process_video(self, input_path, output_path):
+        clip = VideoFileClip(input_path)
+        out_clip = clip.fl_image(self.forward)
+        out_clip.write_videofile(output_path, audio=False)
+
+def main():
+    # args = docopt(__doc__)
+    # input = args['INPUT_PATH']
+    # output = args['OUTPUT_PATH']
+    # input = "/Users/namle/BachKhoa/HK232/DOANAI/Advanced-Lane-Lines/project_video.mp4"
+    # output = "/Users/namle/BachKhoa/HK232/DOANAI/Advanced-Lane-Lines/output2_video.mp4"
+    calibration = CameraCalibration('camera_cal', 9, 6)
+    thresholding = Thresholding()
+    transform = PerspectiveTransformation()
+    lanelines = LaneLines()
+    video = cv2.VideoCapture("/Users/namle/BachKhoa/HK232/DOANAI/Advanced-Lane-Lines/project_video.mp4")
+    # video = cv2.VideoCapture("/Users/namle/BachKhoa/HK232/DOANAI/Advanced-Lane-Lines/video.mp4")
+    video.set(cv2.CAP_PROP_FPS, 10)
+    while (video.isOpened()):
+        try:
+            flag, img = video.read()
+            img_real = np.copy(img)
+            if not flag: break
+            b, g, r = cv2.split(img)
+            img = cv2.merge([r, g, b])
+            out_img = np.copy(img)
+            img = calibration.undistort(img)
+            img = transform.forward(img)
+            bird_eye_img = np.copy(img)
+            img = thresholding.forward(img)
+            img = lanelines.forward(img)
+            img = transform.backward(img)
+            out_img = cv2.addWeighted(out_img, 1, img, 0.6, 0)
+            out_img = lanelines.plot(out_img)
+
+            # cv2.imshow("test",out_img)
+            # cv2.imshow("test2",img)
+            combined_img = np.hstack((out_img, img_real,bird_eye_img))
+            cv2.imshow("Combined Images", combined_img)
+            if cv2.waitKey(1) == ord('q'):break
+        except Exception as e:
+            print(e)
+   
+
+if __name__ == "__main__":
+    main()
